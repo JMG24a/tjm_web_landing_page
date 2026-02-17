c575ede2df499c68982838050c4f3b0ce50d7f2d
jmg24a <jmg24a@gmail.com>
2026-02-16 23:33:10 -0400

    test

--- public/js/router.js
+++ public/js/router.js
@@ -1,56 +1,56 @@
-const routes = {
-  "/": "/home.html",
-  "/products": "/products.html",
-  "/about": "/pages/about.html",
-  "/admin": "/pages/admin.html",
-};
+// const routes = {
+//   "/": "/home.html",
+//   "/products": "/products.html",
+//   "/about": "/pages/about.html",
+//   "/admin": "/pages/admin.html",
+// };
 
-const pageScripts = {
-  "/": initHome,
-  "/products": initProducts,
-  "/admin": initAdmin
-};
+// const pageScripts = {
+//   "/": initHome,
+//   "/products": initProducts,
+//   "/admin": initAdmin
+// };
 
-const app = document.getElementById("app");
+// const app = document.getElementById("app");
 
-function normalizePath(path) {
-  return path.replace(/\/+$/, "") || "/";
-}
+// function normalizePath(path) {
+//   return path.replace(/\/+$/, "") || "/";
+// }
 
-async function navigateTo(url, addToHistory = true) {
-  const parsedUrl = new URL(url, window.location.origin);
-  const path = normalizePath(parsedUrl.pathname);
+// async function navigateTo(url, addToHistory = true) {
+//   const parsedUrl = new URL(url, window.location.origin);
+//   const path = normalizePath(parsedUrl.pathname);
 
-  const route = routes[path] || routes["/"];
+//   const route = routes[path] || routes["/"];
 
-  const res = await fetch(route);
-  const html = await res.text();
+//   const res = await fetch(route);
+//   const html = await res.text();
 
-  app.innerHTML = html;
+//   app.innerHTML = html;
 
-  if (addToHistory) {
-    window.history.pushState({}, "", url);
-  }
+//   if (addToHistory) {
+//     window.history.pushState({}, "", url);
+//   }
 
-  window.scrollTo({ top: 0 });
-  pageScripts[path]?.();
-}
+//   window.scrollTo({ top: 0 });
+//   pageScripts[path]?.();
+// }
 
-function handleLinks() {
-  console.log("INIT")
-  document.body.addEventListener("click", e => {
-    const link = e.target.closest("a[data-link]");
-    if (!link) return;
+// function handleLinks() {
+//   console.log("INIT")
+//   document.body.addEventListener("click", e => {
+//     const link = e.target.closest("a[data-link]");
+//     if (!link) return;
 
-    e.preventDefault();
-    navigateTo(link.getAttribute("href"));
-  });
-}
+//     e.preventDefault();
+//     navigateTo(link.getAttribute("href"));
+//   });
+// }
 
-window.addEventListener("popstate", () => {
-  navigateTo(window.location.pathname + window.location.search, false);
-});
+// window.addEventListener("popstate", () => {
+//   navigateTo(window.location.pathname + window.location.search, false);
+// });
 
-// INIT
-navigateTo(window.location.pathname + window.location.search, false);
-handleLinks();
+// // INIT
+// navigateTo(window.location.pathname + window.location.search, false);
+// handleLinks();
