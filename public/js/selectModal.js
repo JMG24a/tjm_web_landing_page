315c8dd5831f2f9345418a37e0e77227634e935a
Jose Morales <70823209+JMG24a@users.noreply.github.com>
2026-07-24 11:47:00 -0400

    Add initial price and installment calculations

--- public/js/selectModal.js
+++ public/js/selectModal.js
@@ -206,6 +206,8 @@ ${colorLine ? "  " + colorLine + "\n" : ""}
 ${materialLine ? "  " + materialLine + "\n" : ""}
   📦 Cantidad: ${producto.cantidad}
   💵 Precio: $${priceProductWs * producto.cantidad}
+  💵 inicial: $${((priceProductWs * producto.cantidad)/100)*40}
+  💵 3 Cuotas: $${(priceProductWs-(((priceProductWs * producto.cantidad)/100)*40))/3}
 
 📍 *Sede de retiro:* ${locationWsSede}
 
