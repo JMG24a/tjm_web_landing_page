2018f3320e4617c9e56b8f02ade699f2dc3b9385
jmg24a <jmg24a@gmail.com>
2026-07-24 23:17:57 -0400

    saddsa

--- public/js/config.js
+++ public/js/config.js
@@ -16,26 +16,26 @@ categorias.forEach(cat => {
 });
 
 // Render productos por categoría
+  // Render productos por categoría
 function mostrarProductos(categoria) {
   categoriasContainer.classList.add("hidden");
   productosContainer.classList.remove("hidden");
   productosContainer.innerHTML = "";
 
-  const filtrados = productos.filter(p => p.category === categoria);
+  const productos = PRODUCTS[categoria];
 
-  filtrados.forEach(prod => {
+  productos.forEach(prod => {
     const card = document.createElement("div");
     card.className = "product-card";
 
     card.innerHTML = `
-      <img src="${prod.image}" alt="${prod.name}">
+      <img src="img/${prod.img}" alt="${prod.name}">
       <div>
         <h3>${prod.name}</h3>
       </div>
     `;
 
     card.addEventListener("click", () => {
-      // abrir modal o navegar al producto
       openProductModal(prod, categoria);
     });
 
