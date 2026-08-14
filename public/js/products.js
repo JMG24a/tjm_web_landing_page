a50a21f411da4100efb039b8dc538bdd4d8e0b59
jmg24a <jmg24a@gmail.com>
2026-08-14 00:24:16 -0400

    sda

--- public/js/products.js
+++ public/js/products.js
@@ -528,6 +528,38 @@ function setupComedores(product) {
   updateChairs(0);
 }
 
+function setupOfertas(){
+ isFabricCategory()
+  const colors = document.getElementById("modal-colors");
+  const container_size = document.getElementById("container_size");
+  container_size.classList.remove("displayNone");
+
+  container_size.innerHTML = `
+      <p>🪓 Madera Seca al Horno.</p>
+      ${product.atributte.resortes ? "<p>🛌 Resortes Pocket.</p>" : ""}
+      <p>📄 Garantía de 1 año.</p>
+      <p>🕓 30 días hábiles.</p>
+      <p>📦 Embalaje sin cargo.</p>
+      <p class="size">
+        <i>
+          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#434343">
+            <path d="M208-120q-37 0-62.5-25.5T120-208v-548q0-29 27-40.5t47 8.5l90 90-54 54 28 28 54-54 104 104-54 54 28 28 54-54 104 104-54 54 28 28 54-54 104 104-54 54 28 28 54-54 80 80q20 20 8.5 47T756-120H208Zm32-120h332L240-572v332Z"/>
+          </svg>
+        </i>
+        <span id="size" class="size_text"></span>
+      </p>
+    `;
+
+  const size = document.getElementById("size");
+  size.textContent = product.atributte.size || "";
+
+  const price = document.getElementById("product-price");
+  price.classList.remove("displayNone")
+  price.innerHTML = '<span class="loader"></span>'
+  loadProductPrice(product.id, porcentajesPagoMethod.cashea);
+  renderColors(product.colors, colors);
+}
+
 // 3. Toggle USD ↔ Bs
 priceElement.addEventListener("click", async () => {
   const mode = priceElement.dataset.mode;
@@ -616,6 +648,11 @@ function openProductModal(product, category) {
     case "comedores":
       loadPayPercentage(methodPayProductWs)
       setupComedores(product);
+      break;
+
+    case "ofertas":
+      loadPayPercentage(methodPayProductWs)
+      setupOfertas(product);
       break;
 
     case "colchones":
