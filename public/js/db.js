09a62db3b81373a8b49d8548f3d942e997c5aed6
jmg24a <jmg24a@gmail.com>
2026-08-22 23:13:19 -0400

    sad

--- public/js/db.js
+++ public/js/db.js
@@ -46,9 +46,9 @@ const WELCOME = {
     slogan_2: "descansa en tu hogar soñado."
   },
   accesorios:{
-    image4_5: "/image/4_5_dormitorio_slider.jpg",
-    image16_9: "/image/16_9_dormitorio_slider.jpg",
-    image21_9: "/image/21_9_dormitorio_slider.jpg",
+    image4_5: "/image/category_accesorios.jpg",
+    image16_9: "/image/category_accesorios.jpg",
+    image21_9: "/image/category_accesorios.jpg",
     slogan_1: "Crea tu espacio",
     slogan_2: "descansa en tu hogar soñado."
   },
